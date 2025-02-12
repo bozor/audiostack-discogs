aa958b443abe8d0faa474457a85319fbad07a53d
Boris Grudinin <boris.grudinin@omm.agency>
2025-02-12 15:29:23 +0000

    update next config to fix missing images in deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  basePath: "/audiostack-discogs",
+  output: "export",
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
