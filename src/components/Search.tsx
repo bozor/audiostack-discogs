4a06a4f08a8106f1082d9bf8a242e05cab56b683
Boris Grudinin <boris.grudinin@omm.agency>
2025-02-13 17:21:27 +0000

    add in basic artist search that outputs results to console

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,8 +1,19 @@
+'use client'
+import { useState } from 'react';
+
+import { search } from '@/app/actions';
+
 const Search = () => {
+  const [artist, setArtist] = useState('');
+  const handleClick = async () => {
+    const result = await search(artist);
+
+    console.log(result)
+  }
   return (
     <div>
-      <input type="text" />
-      <button>search</button>
+      <input type="text" value={artist} onChange={(event) => setArtist(event.currentTarget.value)} />
+      <button onClick={handleClick}>search</button>
     </div>
   )
 }
