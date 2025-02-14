0c58b9c771ea8f42a194810c5705bd7d74f4095e
Boris Grudinin <boris.grudinin@omm.agency>
2025-02-14 12:12:41 +0000

    add styling for release page

--- src/components/PageRelease.tsx
+++ src/components/PageRelease.tsx
@@ -7,6 +7,8 @@ import { fetcher } from "@/utils/fetcher";
 
 import Release from "./Release";
 
+import styles from './PageRelease.module.css'
+
 const getImage = (image: Record<string, string | number>) => {
   if (image) {
     return {
@@ -26,9 +28,9 @@ const PageRelease = () => {
   const { data, error, isLoading } = useSWR(id ? `https://api.discogs.com/releases/${id}?token=${process.env.NEXT_PUBLIC_DISCOGS_TOKEN}`: null, fetcher)
 
   return (
-    <div>
-      {error && <span>An error occured, refresh and try again</span>}
-      {isLoading && <span>loading</span>}
+    <div className={styles.wrap}>
+      {error && <span className={styles.error}>An error occured, refresh and try again</span>}
+      {isLoading && <span className={styles.loading}>loading</span>}
       {data && (
         <Release 
           have={data.community.have || 0}
